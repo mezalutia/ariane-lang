39d3b8cf263079883e21957447cad6f79625126b
Caotinification <nguyenjustin930@gmail.com>
2025-10-13 15:26:36 -0400

    renaming some variables

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -34,27 +34,27 @@ impl Lexer {
         let mut reader = BufReader::new(file);
         let mut chars = reader.chars().peekable();
         let mut str_buffer = String::new();
-        let mut tokens_list: Vec<Token> = Vec::new();
+        let mut tokens: Vec<Token> = Vec::new();
         let mut curr_char_option = chars.next();
         while let Some(Ok(curr_char)) = curr_char_option {
             match curr_char {
                 'a'..='z' | 'A'..='Z' | '0'..='9' => {
                     str_buffer.push(curr_char);
                     if let Some(Ok(next_char)) = chars.peek() && !next_char.is_alphanumeric() {
-                        tokens_list.push(Lexer::determine_token(&str_buffer));
+                        tokens.push(Lexer::determine_token(&str_buffer));
                         str_buffer.clear();
                     }
                 }
-                '=' => tokens_list.push(Token::Assignment),
-                ';' => tokens_list.push(Token::Semicolon),
-                ':' => tokens_list.push(Token::Colon),
+                '=' => tokens.push(Token::Assignment),
+                ';' => tokens.push(Token::Semicolon),
+                ':' => tokens.push(Token::Colon),
                 ' ' | '\n' => (),
-                _ => tokens_list.push(Token::Unknown),
+                _ => tokens.push(Token::Unknown),
             }
             curr_char_option = chars.next();
         }
-        tokens_list.push(Token::EOF);
-        tokens_list
+        tokens.push(Token::EOF);
+        tokens
     }
     fn determine_token(id: &String) -> Token {
         match id.as_str() {
